552cdeeebdfb3a224e78276b566ff198c88a10f4
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2020-11-04 11:27:52 +0000

    Add DB conn

--- main/settings.py
+++ main/settings.py
@@ -134,3 +134,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
