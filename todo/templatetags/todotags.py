d67b40de02d546041d7be898a227291f155d0136
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2020-11-04 11:42:54 +0000

    Add todo templatetags

---
+++ todo/templatetags/todotags.py
@@ -0,0 +1,23 @@
+from django import template
+
+
+register = template.Library()
+
+SIZES = {
+    1: ('XS', 'xtrasmall'),
+    2: ('S', 'small'),
+    3: ('M', 'medium'),
+    4: ('L', 'large'),
+    5: ('XL', 'xtralarge'),
+}
+
+@register.filter()
+def as_task_size(size_level, format="short"):
+    if size_level < 0 or size_level >5:
+        return size_level
+    t = SIZES.get(size_level)
+    if format == 'long':
+        index = 1
+    else:
+        index = 0
+    return t[index]
